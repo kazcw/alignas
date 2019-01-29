6067338bf1511946e7b634a64d3b4cf3e8d974e9
Kaz Wesley <kaz@lambdaverse.org>
2019-01-29 09:43:41 -0800

    pass through common traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![no_std]
+use core::cmp::{Ord, Ordering, PartialOrd};
+use core::fmt::{Debug, Display, Formatter, Result};
+use core::hash::{Hash, Hasher};
 use core::ops::{Deref, DerefMut};
 
 /// Wrap an object of type T to give it the alignment requirements of an object of type A.
@@ -22,12 +25,14 @@ use core::ops::{Deref, DerefMut};
 /// ints.copy_from_slice(unsafe { slice::from_raw_parts(ptr, 8) });
 /// ```
 #[repr(C)]
+#[derive(Copy, Clone)]
 pub union AlignAs<T: Copy, A: Copy> {
     t: T,
     _marker: A,
 }
 
 impl<T: Copy, A: Copy> AlignAs<T, A> {
+    /// Put the given T object into an aligned location
     #[inline]
     pub fn new(t: T) -> Self {
         AlignAs { t }
@@ -46,5 +51,53 @@ impl<T: Copy, A: Copy> DerefMut for AlignAs<T, A> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
         unsafe { &mut self.t }
+    }
+}
+
+impl<T: Default + Copy, A: Copy> Default for AlignAs<T, A> {
+    #[inline]
+    fn default() -> Self {
+        Self::new(T::default())
+    }
+}
+
+impl<T: Debug + Copy, A: Copy> Debug for AlignAs<T, A> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        self.deref().fmt(f)
+    }
+}
+
+impl<T: Display + Copy, A: Copy> Display for AlignAs<T, A> {
+    #[inline]
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        self.deref().fmt(f)
+    }
+}
+
+impl<T: PartialEq + Copy, A: Copy> PartialEq for AlignAs<T, A> {
+    #[inline]
+    fn eq(&self, rhs: &Self) -> bool {
+        self.deref().eq(rhs)
+    }
+}
+impl<T: Eq + Copy, A: Copy> Eq for AlignAs<T, A> {}
+
+impl<T: PartialOrd + Copy, A: Copy> PartialOrd for AlignAs<T, A> {
+    #[inline]
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        self.deref().partial_cmp(rhs)
+    }
+}
+impl<T: Ord + Copy, A: Copy> Ord for AlignAs<T, A> {
+    #[inline]
+    fn cmp(&self, rhs: &Self) -> Ordering {
+        self.deref().cmp(rhs)
+    }
+}
+
+impl<T: Hash + Copy, A: Copy> Hash for AlignAs<T, A> {
+    fn hash<H: Hasher>(&self, h: &mut H) {
+        self.deref().hash(h)
     }
 }
